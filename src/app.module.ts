16ca00bcc09f0edafdfcfce68c2df3efa16706e8
eoKelvio <kelvinsc7@gmail.com>
2025-09-28 16:36:56 -0300

    :sparkles: feat: posts endpoint create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PostsModule],
   controllers: [],
   providers: [PrismaService],
 })
