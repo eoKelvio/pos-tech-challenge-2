3a8ff8c99cd43f401038ae881247f9c3d891d454
eoKelvio <kelvinsc7@gmail.com>
2025-09-28 18:22:09 -0300

    :wrench: chore: modify actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PostsModule } from './posts/posts.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [AuthModule, PostsModule],
+  imports: [AuthModule, PostsModule, HealthModule],
   controllers: [],
   providers: [PrismaService],
 })
